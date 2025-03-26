81d2483302bc3c7597b687c5ccb3bfefd0b841bb
Gargee Thakur <gargeet16@gmail.com>
2025-03-26 23:38:58 +0530

    Add files via upload

---
+++ database/CustomerDetails.sql
@@ -0,0 +1,5 @@
+use TravelManagementSystem;
+
+create table customer(username varchar(20), id varchar(30), number varchar(30), name varchar(30), gender varchar(10), country varchar(30), address varchar(50), phone varchar(15), email varchar(40));
+
+select * from customer;
---
+++ database/SignUp.sql
@@ -0,0 +1,9 @@
+create database TravelManagementSystem;
+
+use TravelManagementSystem;
+
+create table account(username varchar(20), name varchar(30), password varchar(20), security varchar(50), answer varchar(50));
+
+show tables;
+
+select * from account;
